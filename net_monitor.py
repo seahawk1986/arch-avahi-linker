3ce4ab35bced886368c8f6fa7e1d822cd956b450
seahawk1986 <seahawk1986@hotmail.com>
2013-12-16 17:38:44 +0100

    Revert "new net_monitor version"
    This reverts commit 9899523e4ad6ebdf1ad8fdcb752b44d50d044a90.

--- net_monitor.py
+++ net_monitor.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python3
-import dbus
-import datetime
-import socket
 from socketserver import UDPServer, BaseRequestHandler
+import argparse
+import dbus
+import socket
+import datetime
+
 
 class Handler(BaseRequestHandler):
         def handle(self):
-            print("message:", self.request[0])
-            print("from:", self.client_address)
-            r_hostname, message = self.request[0].decode('utf-8').split(':')
-            if message == 'update' and hostname != r_hostname:
+            #print("message:", self.request[0])
+            #print("from:", self.client_address)
+            #print(self.request[0].decode('utf-8'))
+            #print(self.client_address[0] in ipList)
+            hostname, message = self.request[0].decode('utf-8').split(':')
+            if (message == 'update' and not hostname == socket.gethostname()):
                 bus = dbus.SystemBus()
                 dbus2vdr = bus.get_object('de.tvdr.vdr', '/Recordings')
                 answer = dbus.Int32(0)
@@ -20,10 +24,14 @@ class Handler(BaseRequestHandler):
                 socket = self.request[1]
                 socket.sendto(bytes("okily dokily",'UTF-8'), self.client_address)
             else:
-                print("ignoring message from own hostname {0}".format(r_hostname))
+                print("ignoring local ip address {0}".format(self.client_address[0]))
 
-addr = ("", 5555)
-hostname = socket.gethostname()
+argparser = argparse.ArgumentParser(description='update vdr recdir on UDP message')
+argparser.add_argument('-p', '--port',  metavar='PORT', type=int,
+                                              dest='port', default=5555, help='udp port (default 5555)')
+args = vars(argparser.parse_args())
+
+addr = ("",  args['port'])
 print("listening on %s:%s" % addr)
 server = UDPServer(addr, Handler)
 server.serve_forever()
